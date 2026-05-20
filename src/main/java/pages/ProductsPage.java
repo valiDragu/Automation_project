c4841d225fbf80678328616c891d48823db47cc6
valiDragu <valismd2393+git@gmail.com>
2026-05-20 18:28:18 +0300

    test

--- src/main/java/pages/ProductsPage.java
+++ src/main/java/pages/ProductsPage.java
@@ -3,15 +3,18 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ProductsPage {
     private WebDriver driver;
 
-    private By filterOptions = By.xpath("//select[@data-test='product-sort-container']");
-    private By activeFilterName = By.xpath("//span[@data-test='active-option']");
+    private By filterBtn = By.xpath("//select[@data-test='product-sort-container']");
+    private By filterName = By.xpath("//span[@data-test='active-option']");
     private By productName = By.xpath("//div[@data-test='inventory-item-name']");
     private By productPrice = By.xpath("//div[@data-test='inventory-item-price']");
     private By productDescription = By.xpath("//div[@data-test='inventory-item-desc']");
@@ -22,17 +25,13 @@ public class ProductsPage {
         this.driver = driver;
     }
 
-    public void selectFilterOption(String option) {
-        Select select = new Select(driver.findElement(filterOptions));
+    public void selectFilter(String option) {
+        Select select = new Select(driver.findElement(filterBtn));
         select.selectByVisibleText(option);
     }
 
-    public String getActiveFilterName() {
-        return driver.findElement(activeFilterName).getText().trim();
-    }
-
-    public String getProductDescription(String productName) {
-        return xpathHelper(productName).findElement(productDescription).getText().trim();
+    public String getFilterName() {
+        return driver.findElement(filterName).getText().trim();
     }
 
     public String getProductName(int itemIndex) {
@@ -41,23 +40,67 @@ public class ProductsPage {
         return driver.findElement(By.xpath(indexedProductName)).getText().trim();
     }
 
-    public String getProductPrice(String productName) {
-        return xpathHelper(productName).findElement(productPrice).getText().trim();
+    public String getProductDescription(int itemIndex) {
+        String productDescriptionPath = productDescription.toString().replace("By.xpath: ", "");
+        String indexedProductDescription = "(" + productDescription + ")[" + itemIndex + "]";
+        return driver.findElement(By.xpath(indexedProductDescription)).getText().trim();
     }
 
-    public void addProductToCart(String productName) {
-        xpathHelper(productName).findElement(addToCartBtn).click();
+    public double getProductPrice(int itemIndex) {
+        String productPricePath = productPrice.toString().replace("By.xpath: ", "");
+        String indexedProductPrice = "(" + productPricePath + ")[" + itemIndex + "]";
+        String price = driver.findElement(By.xpath(indexedProductPrice)).getText().trim();
+        price.replace("$", "");
+        double finalPrice = Double.parseDouble(price);
+        return finalPrice;
     }
 
-    public void removeProductFromCart(String productName) {
-        xpathHelper(productName).findElement(removeFromCartBtn).click();
+    public void addProductToCart(int itemIndex) {
+        String productCartBtn = addToCartBtn.toString().replace("By.xpath: ", "");
+        String indexedCartBtn = "(" + addToCartBtn + ")[" + itemIndex + "]";
     }
 
-
-    private WebElement xpathHelper(String productName) {
-        String xpath = "//div[@data-test='inventory-item'][.//div[text()='" + productName + "']]";
-        return driver.findElement(By.xpath(xpath));
+    public void removeProductFromCart(int itemIndex) {
+        String productCartBtnR = removeFromCartBtn.toString().replace("By.xpath: ", "");
+        String indexedCartBtnR = "(" + removeFromCartBtn + ")[" + itemIndex + "]";
     }
 
+    public int getProductsCount() {
+        List <WebElement> totalProducts = driver.findElements(productName);
+        return totalProducts.size();
+    }
 
+    public List <String> getAllProductsNames() {
+        List <String> productNames = new ArrayList<>();
+        List <WebElement> products = driver.findElements(productName);
+        for (WebElement product : products) {
+            productNames.add(product.getText().trim());
+        }
+        return productNames;
+    }
+
+    public List <String> fiterNamesASC() {
+        List <String> namesASC = new ArrayList<>(getAllProductsNames());
+        Collections.sort(namesASC);
+        return namesASC;
+    }
+
+    public List<Double> getAllProductsPrices() {
+        List <Double> productPrices = new ArrayList<>();
+        List<WebElement> prices = driver.findElements(productPrice);
+
+        for(WebElement price : prices) {
+            String stringPrice = price.getText().trim();
+            String cutPrice = stringPrice.replace("$", "");
+            Double doublePrice = Double.parseDouble(cutPrice);
+            productPrices.add(doublePrice);
+        }
+        return productPrices;
+    }
+
+    public List<Double> filterPriceASC() {
+        List <Double> pricesASC = new ArrayList<>(getAllProductsPrices());
+        Collections.sort(pricesASC);
+        return  pricesASC;
+    }
 }
--- src/test/java/TestUI.java
+++ src/test/java/TestUI.java
@@ -5,6 +5,9 @@ import pages.BasePage;
 import pages.LoginPage;
 import pages.ProductsPage;
 
+import java.util.Collections;
+import java.util.List;
+
 public class TestUI extends BaseTest {
 
     @Test
@@ -40,15 +43,72 @@ public class TestUI extends BaseTest {
         Assert.assertEquals(basePage.getPageName(), "Products");
 
         ProductsPage productsPage = new ProductsPage(driver);
-        productsPage.selectFilterOption("Price (low to high)");
+        productsPage.selectFilter("Name (Z to A)");
 
-        String firstProduct = productsPage.getProductName(1);
-        String firstProductPrice = productsPage.getProductPrice(firstProduct);
-        String secondProduct = productsPage.getProductName(2);
-        String secondProductPrice = productsPage.getProductPrice(secondProduct);
+        List<String> actualDisplayedProducts = productsPage.getAllProductsNames();
+        List<String> expectedDisplayedProducts = productsPage.fiterNamesASC();
+        Collections.sort(expectedDisplayedProducts, Collections.reverseOrder());
 
-        Assert.assertTrue(firstProductPrice.compareTo(secondProductPrice) <= 0, "Low-to-high filter not working");
+        Assert.assertEquals(actualDisplayedProducts, expectedDisplayedProducts, "Filter error");
+    }
 
+    @Test
+    public void Test4() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.sendUsername("standard_user");
+        loginPage.sendPassword("secret_sauce");
+        loginPage.login();
 
+        BasePage basePage = new BasePage(driver);
+        Assert.assertEquals(basePage.getPageName(), "Products");
+
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.selectFilter("Name (A to Z)");
+
+        List<String> actualDisplayedProducts = productsPage.getAllProductsNames();
+        List<String> expectedDisplayedProducts = productsPage.fiterNamesASC();
+        Collections.sort(expectedDisplayedProducts);
+
+        Assert.assertEquals(actualDisplayedProducts, expectedDisplayedProducts, "Filter error");
+    }
+
+    @Test
+    public void Test5() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.sendUsername("standard_user");
+        loginPage.sendPassword("secret_sauce");
+        loginPage.login();
+
+        BasePage basePage = new BasePage(driver);
+        Assert.assertEquals(basePage.getPageName(), "Products");
+
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.selectFilter("Price (low to high)");
+
+        List<Double> actualDisplayedPrices = productsPage.getAllProductsPrices();
+        List<Double> expectedDisplayedPrices = productsPage.filterPriceASC();
+        Collections.sort(expectedDisplayedPrices);
+
+        Assert.assertEquals(actualDisplayedPrices, expectedDisplayedPrices, "Filter error");
+    }
+
+    @Test
+    public void Test6() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.sendUsername("standard_user");
+        loginPage.sendPassword("secret_sauce");
+        loginPage.login();
+
+        BasePage basePage = new BasePage(driver);
+        Assert.assertEquals(basePage.getPageName(), "Products");
+
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.selectFilter("Price (high to low)");
+
+        List<Double> actualDisplayedPrices = productsPage.getAllProductsPrices();
+        List<Double> expectedDisplayedPrices = productsPage.filterPriceASC();
+        Collections.sort(expectedDisplayedPrices, Collections.reverseOrder());
+
+        Assert.assertEquals(actualDisplayedPrices, expectedDisplayedPrices, "Filter error");
     }
 }
